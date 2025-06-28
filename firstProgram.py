d23aecbc93f7f82faadd590344a054415f58ad90
nomanmujahid1144 <nomanmujahid.cs@gmail.com>
2025-06-28 22:10:39 +0500

    first commit

---
+++ firstProgram.py
@@ -0,0 +1,16 @@
+
+# Do numbers ka sum karne ka simple Python program
+
+# User se input lena
+number1 = float(input("Pehla number dain: "))
+number2 = float(input("Dosra number dain: "))
+
+# Dono numbers ka sum karna
+sum = number1 + number2
+
+# Result print karna
+print("Dono numbers ka sum hai:", sum)
+
+
+
+print("Hello, World! Python is running successfully.")
