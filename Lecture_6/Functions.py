265ee0f30433fe169844305e8e861865071c4d24
Hannan Mujahid <hannimujahid37@gmail.com>
2025-09-28 01:17:26 +0500

    update now

--- Lecture_6/Functions.py
+++ Lecture_6/Functions.py
@@ -1,3 +1,5 @@
+
+"""
 def calc_sum(a,b):
     sum = a + b
     print("Sum is: ",sum)
@@ -11,3 +13,10 @@ calc_sum(15,25)
 # more lines of code
 
 calc_sum(100,250)
+"""
+
+def calc_sum(a,b):
+    return a + b
+
+sum = calc_sum(5,10)
+print("Sum is: ",sum)
