d847cbd1326840f0af979c49689f545d053478cc
Hannan Mujahid <hannimujahid37@gmail.com>
2025-09-28 01:20:06 +0500

    update now

--- Lecture_6/Functions.py
+++ Lecture_6/Functions.py
@@ -15,8 +15,8 @@ calc_sum(15,25)
 calc_sum(100,250)
 """
 
-def calc_sum(a,b):
+def calc_sum(a,b):      # Function Definition
     return a + b
 
-sum = calc_sum(5,10)
+sum = calc_sum(5,10)      # Function Call
 print("Sum is: ",sum)
