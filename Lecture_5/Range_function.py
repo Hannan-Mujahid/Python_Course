fd0d2068be24e8c24068999cf0566592c86cde93
Hannan Mujahid <hannimujahid37@gmail.com>
2025-09-27 12:08:18 +0500

    updates

---
+++ Lecture_5/Range_function.py
@@ -0,0 +1,27 @@
+
+# Print numbers from  1 to 100
+
+"""
+for i in range(5,):
+    print(i)
+"""
+
+# Print numbers from  100 to 1
+
+"""
+for i in range(100,0,-1):
+    print(i)
+"""
+
+
+# print the multiplication table of a given number
+"""
+nums = int(input("Enter a number to print its table: "))
+for i in range(1, 11):
+    print( nums, "x", i, "=", nums*i)
+"""
+
+
+
+
+
