8c02360f018971896ff8107eb3782accf9ff66c3
Hannan Mujahid <hannimujahid37@gmail.com>
2025-09-27 13:05:32 +0500

    updates file

--- Lecture_5/Practice_Questions.py
+++ Lecture_5/Practice_Questions.py
@@ -35,3 +35,27 @@ while n < len(nums):
         print("Finding....")
     n +=1
 """
+
+"""
+n = int(input("Enter a number to fine sum of n natural numbers: "))
+sum = 0
+for i in range(1,n+1):
+    sum +=i
+
+print("total sum = ",sum)
+"""
+
+
+n = int(input("Enter a number to find natural: "))
+sum = 0
+i=1
+while i <=n:
+    sum +=i
+    i +=1
+    print("total sum = ",sum)
+    
+
+
+
+
+
--- Lecture_5/pass_function.py
+++ Lecture_5/pass_function.py
@@ -2,3 +2,7 @@
 for el in range(5):
    pass
 
+if el >= 5:
+   pass
+
+print("some useful work")
