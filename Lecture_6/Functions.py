5fad3566a46ff0d37b89d5598b4765b81461b4e9
Hannan Mujahid <hannimujahid37@gmail.com>
2025-09-28 15:45:59 +0500

    updates

--- Lecture_6/Functions.py
+++ Lecture_6/Functions.py
@@ -21,13 +21,22 @@ def calc_sum(a,b):      # Function Definition; parameters
 sum = calc_sum(5,10)      # Function Call; arguments
 print("Sum is: ",sum)
 """
-
+"""
 def print_hello():
     print("Hello")
     
 out_put = print_hello()
 print(out_put)
+"""
 
+
+def calc_aveg(a,b,c):
+    sum = a+b+c 
+    average = sum / 3
+    print("Sum is: ",sum)
+    print("Average is: ",average)
+
+calc_aveg(5,10,15)
 
 
 
