5780aedb3e25cb9c94a7503334c3337c41449730
Hannan Mujahid <hannimujahid37@gmail.com>
2025-09-30 15:02:51 +0500

    update

--- Practice_Questions _for_functions.py
+++ Practice_Questions _for_functions.py
@@ -1,3 +1,19 @@
+#cricketer_name  = ["Babar Azam", "Virat Kohli", "Steve Smith", "Kane Williamson"]
+#len(cricketer_name)
+#print(len(cricketer_name))
+
+
+
+
+
+
+
+
+
+
 cricketer_name  = ["Babar Azam", "Virat Kohli", "Steve Smith", "Kane Williamson"]
-len(cricketer_name)
-print(len(cricketer_name))
+
+def print_names(list):
+    print("Total number of cricketers: ", len(list))
+
+print_names(cricketer_name)
