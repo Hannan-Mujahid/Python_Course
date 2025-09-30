a0a5c1a78753caa049bdb70c9d297798874e5e5e
Hannan Mujahid <hannimujahid37@gmail.com>
2025-09-30 15:32:07 +0500

    update

--- Practice_Questions _for_functions.py
+++ Practice_Questions _for_functions.py
@@ -26,6 +26,16 @@ def print_hero(list):
 print_hero(heroes)
 """
 
+# WAF to find the factorial of a number. (number is the parameter)
+# Factorial of 3 = 3*2*1 = 6
 
+"""
+n = 3
+def calc_factorial(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    print("Factorial of ", n, " is: ", fact)
 
-
+calc_factorial(n)
+"""
