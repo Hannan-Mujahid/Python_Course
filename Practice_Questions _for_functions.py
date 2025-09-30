e8bc08288bb66a674d19916bc3b61795f2e8df47
Hannan Mujahid <hannimujahid37@gmail.com>
2025-09-30 15:20:29 +0500

    update

--- Practice_Questions _for_functions.py
+++ Practice_Questions _for_functions.py
@@ -3,17 +3,29 @@
 #print(len(cricketer_name))
 
 
+#    WAF to print the length of a list . (list is the parameter)
 
-
-
-
-
-
-
-
+"""
 cricketer_name  = ["Babar Azam", "Virat Kohli", "Steve Smith", "Kane Williamson"]
 
 def print_names(list):
     print("Total number of cricketers: ", len(list))
 
 print_names(cricketer_name)
+"""
+
+
+# WAF to print the elements of a list in a single line . (list is the parameter)
+"""
+heroes = ["Superman", "Batman", "Wonder", "Flash",]
+
+def print_hero(list):
+    for hero in list:
+        print(hero, end=", ")
+
+print_hero(heroes)
+"""
+
+
+
+
