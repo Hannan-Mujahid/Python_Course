85d1913f5555b4273f3170728e8e50ccf00d410c
Hannan Mujahid <hannimujahid37@gmail.com>
2025-10-06 21:43:32 +0500

    update

--- Lecture_7/File_I/File_I/File_topic.py
+++ Lecture_7/File_I/File_I/File_topic.py
@@ -1,4 +1,12 @@
 f = open("demo.txt", "r")
 data = f.read()
 print(data)
+
+again_wright = input("Do you want to write again (y/n): ")
+if again_wright == 'y':
+    f = open("demo.txt", "a")
+    data = input("Enter your data: ")
+    f.write(data)
+    f.write("\n")
 f.close()
+# f.close()
