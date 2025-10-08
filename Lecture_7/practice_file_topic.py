49679c2b3bcf12401c3e42ad167884a0d8aedfec
Hannan Mujahid <hannimujahid37@gmail.com>
2025-10-08 18:42:09 +0500

    update

--- Lecture_7/practice_file_topic.py
+++ Lecture_7/practice_file_topic.py
@@ -1,6 +1,17 @@
+
+# Create a new file "practice.txt", using python . Add the following data in it
+"""
+Hi everyone 
+we are learning file I/O 
+using python 
+I like a programming language.
+"""
+# first Method
+"""
 f = open("practice.txt", "w" )
 f.write("Hi everyone \nwe are learning file I/O \nusing python \nI like a programming language.")
 f.close()
 f = open("practice.txt", "r" )
 print(f.read())
 f.close()
+"""
