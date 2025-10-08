9b7d628d492d08d43e8e9699c921f212a393f15d
Hannan Mujahid <hannimujahid37@gmail.com>
2025-10-08 18:38:02 +0500

    new file

---
+++ Lecture_7/practice_file_topic.py
@@ -0,0 +1,6 @@
+f = open("practice.txt", "w" )
+f.write("Hi everyone \nwe are learning file I/O \nusing python \nI like a programming language.")
+f.close()
+f = open("practice.txt", "r" )
+print(f.read())
+f.close()
