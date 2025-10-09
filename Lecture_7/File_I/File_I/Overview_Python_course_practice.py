c8d3aa7309cd2ca589797e42dd3a02ef4cee1429
Hannan Mujahid <hannimujahid37@gmail.com>
2025-10-09 21:24:20 +0500

    update

--- Lecture_7/File_I/File_I/Overview_Python_course_practice.py
+++ Lecture_7/File_I/File_I/Overview_Python_course_practice.py
@@ -18,7 +18,7 @@ fahrenheit = (celsius * 9/5) + 32
 print("Temperature in fahrenheit is: ", fahrenheit)
 """
 # 04
-
+"""
 num = int(input("Enter a number: "))
 if (num >= 1 ):
     print("The number is positive.", num)
@@ -26,3 +26,10 @@ elif(num < 1):
     print("The number is negative.", num)
 else:
     print("The number is zero.", num)
+"""
+# 05
+with open("Test_Python.txt","w") as f:
+    f.write("23,45,67")
+    with open("Test_Python.txt","r") as f:
+        data = f.read()
+        print(data)
