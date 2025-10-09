e1f0b3cdd7e235665367d99819ce5b87c4d0e79e
Hannan Mujahid <hannimujahid37@gmail.com>
2025-10-09 12:57:17 +0500

    no update

--- Lecture_7/File_I/File_I/practice_file_topic.py
+++ Lecture_7/File_I/File_I/practice_file_topic.py
@@ -34,7 +34,7 @@ new_data = data.replace("Python","Java")
 print(new_data)
 """
 # second Method
-
+"""
 with open("practice.txt", "r") as f:    
     data = f.read()
     new_data = data.replace("Python","Java")
@@ -42,4 +42,6 @@ with open("practice.txt", "r") as f:
    
     with open("practice.txt", "w") as f:
         f.write(new_data)
+"""
+
 
