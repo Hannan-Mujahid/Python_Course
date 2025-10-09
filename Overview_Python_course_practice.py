5fb06e58a320e02f81d2fe3a699d28c493184dc1
Hannan Mujahid <hannimujahid37@gmail.com>
2025-10-09 17:39:45 +0500

    update

--- Overview_Python_course_practice.py
+++ Overview_Python_course_practice.py
@@ -5,4 +5,9 @@ h = input("Enter a number: ")
 sum = n + int(h)
 print("The sum is: ", sum)
 """
-    
+# 02
+"""
+name = input("Enter your name: ")
+age = int(input("Enter your age:"))
+print("Hello! ", name ,"you are  ", age," years old.")
+"""
